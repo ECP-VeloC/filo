e2cf7a730c6a1be5c5084338ddb1a5c1e77df6d6
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2020-10-06 13:29:47 -0700

    Update test/test_filo_async.c
    Co-authored-by: Cameron Stanavige <stanavige1@llnl.gov>

--- test/test_filo_async.c
+++ test/test_filo_async.c
@@ -95,7 +95,7 @@ int main(int argc, char* argv[])
   rc = Filo_Flush_start("mapfile", NULL, 1, filelist, dest_filelist, MPI_COMM_NULL, "pthread");
   if(rc == FILO_SUCCESS){
     printf ("Error in line %d, file %s, function %s.\n", __LINE__, __FILE__, __func__);
-    printf("Filo_Flush_start succeded with MPI_COMM_NULL comm parameters\n");
+    printf("Filo_Flush_start succeeded with MPI_COMM_NULL comm parameters\n");
     return TEST_FAIL;
   }
   rc = Filo_Flush_test("mapfile", MPI_COMM_NULL);
